271e217499a3bab03a0664fddda9b944a4cf13eb
Steve Schoonooghe <Steve.Schoonooghe@gmail.com>
2015-10-22 15:10:37 +0200

    Theorie service beans

--- groenetenen/src/main/java/be/vdab/services/FiliaalService.java
+++ groenetenen/src/main/java/be/vdab/services/FiliaalService.java
@@ -5,11 +5,17 @@ import java.util.List;
 import be.vdab.entities.Filiaal;
 
 public interface FiliaalService {
-  void create(Filiaal filiaal); 
-  Filiaal read(long id);
-  void update(Filiaal filiaal);
-  void delete(long id);
-  List<Filiaal> findAll();
-  long findAantalFilialen();
+  
+	void create(Filiaal filiaal); 
+	
+	Filiaal read(long id);
+	
+	void update(Filiaal filiaal);
+	
+	void delete(long id);
+	
+	List<Filiaal> findAll();
+	
+	long findAantalFilialen();
   
 } 
--- groenetenen/src/main/java/be/vdab/web/FiliaalController.java
+++ groenetenen/src/main/java/be/vdab/web/FiliaalController.java
@@ -2,9 +2,13 @@ package be.vdab.web;
 
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import be.vdab.services.FiliaalService;
 
 @Controller 
 @RequestMapping("/filialen")
@@ -13,11 +17,17 @@ class FiliaalController {
 	private static final String TOEVOEGEN_VIEW = "filialen/toevoegen";
 	private static final String REDIRECT_URL_NA_TOEVOEGEN = "redirect:/filialen";
 	private static final Logger logger = Logger.getLogger(FiliaalController.class.getName());
+	private final FiliaalService filiaalService;
 	
-	@RequestMapping(method = RequestMethod.GET)
-	String findAll() {
-		return FILIALEN_VIEW;
+	@Autowired 
+	FiliaalController(FiliaalService filiaalService) {  // Spring injecteert de parameter filiaalService met de bean die de interface
+		this.filiaalService = filiaalService;			// FiliaalService implementeert: FiliaalServiceImpl
 	} 
+	
+	@RequestMapping(method = RequestMethod.GET) 
+	ModelAndView findAll() {
+	  return new ModelAndView(FILIALEN_VIEW, "filialen", filiaalService.findAll());
+	}
   
 	@RequestMapping(path = "toevoegen", method = RequestMethod.GET)
 	String createForm() {
