6862741b5bb42959200f86fa5cd291036128f1cd
Steve Schoonooghe <Steve.Schoonooghe@gmail.com>
2015-10-27 15:51:56 +0100

    JDBC tot p92 in cursus

--- groenetenen/src/main/java/be/vdab/dao/FiliaalDAOImpl.java
+++ groenetenen/src/main/java/be/vdab/dao/FiliaalDAOImpl.java
@@ -1,28 +1,31 @@
 package be.vdab.dao;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import be.vdab.entities.Filiaal;
-import be.vdab.valueobjects.Adres;
 import be.vdab.valueobjects.PostcodeReeks;
 
 @Repository 
 class FiliaalDAOImpl implements FiliaalDAO { 
 	private final Map<Long, Filiaal> filialen = new ConcurrentHashMap<>(); 
-	FiliaalDAOImpl() { 
-		filialen.put(1L, new Filiaal(1, "Andros", true, BigDecimal.valueOf(1000), new Date(), new Adres("Keizerslaan", "11", 1000, "Brussel")));
-		filialen.put(2L, new Filiaal(2, "Delos", false, BigDecimal.valueOf(2000), new Date(), new Adres("Gasthuisstraat", "31", 1000, "Brussel")));
-		filialen.put(3L, new Filiaal(3, "Gavdos", false, BigDecimal.valueOf(3000), new Date(), new Adres("Koestraat", "44", 9700, "Oudenaarde")));
+	private final JdbcTemplate jdbcTemplate;
+	private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	
+	@Autowired
+	FiliaalDAOImpl(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+		  this.jdbcTemplate = jdbcTemplate;
+		  this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
 	} 
-  
+	  
 	@Override
 	public void create(Filiaal filiaal) {
 		filiaal.setId(Collections.max(filialen.keySet()) + 1); 
