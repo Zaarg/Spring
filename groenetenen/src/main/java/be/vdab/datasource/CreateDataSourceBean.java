13c4c8be2b6b3833efb2ceb1ada0371a81b2f1cf
Steve Schoonooghe <Steve.Schoonooghe@gmail.com>
2015-11-06 11:45:19 +0100

    Openshift: datasource conditioneel

--- groenetenen/src/main/java/be/vdab/datasource/CreateDataSourceBean.java
+++ groenetenen/src/main/java/be/vdab/datasource/CreateDataSourceBean.java
@@ -1,15 +1,35 @@
 package be.vdab.datasource;
 
 import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
-public class CreateDataSourceBean { 
-  @Bean
-  DataSource dataSource() {
-    return new JndiDataSourceLookup().getDataSource("jdbc/groenetenen");   
-  }
-  
-} 
+@PropertySource("classpath:/dao.properties")
+public class CreateDataSourceBean {
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	@Autowired
+	private Environment environment;
+
+	@Bean
+	DataSource dataSource() {
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setDriverClassName(environment.getProperty("local.driverClassName"));
+		dataSource.setJdbcUrl(environment.getProperty("local.jdbcURL"));
+		dataSource.setUsername(environment.getProperty("local.userName"));
+		dataSource.setPassword(environment.getProperty("local.password"));
+		return dataSource;
+	}
+}
